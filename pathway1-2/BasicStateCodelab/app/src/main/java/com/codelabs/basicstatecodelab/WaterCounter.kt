320e03b9a40dde84b42342b752fd7b786c09c622
kangmingu93 <kangmingu93@gmail.com>
2022-12-13 22:12:41 +0900

    10. 목록 사용

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -5,23 +5,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
-@Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
-
-        if (count > 0) {
-            Text("You've had $count glasses.")
-        }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
-            Text("Add one")
-        }
-    }
-}
 
 @Composable
 fun StatelessCounter(
@@ -45,17 +30,11 @@ fun StatelessCounter(
 
 @Composable
 fun StatefulCounter(modifier: Modifier = Modifier) {
-    var waterCount by remember { mutableStateOf(0) }
-    var juiceCount by remember { mutableStateOf(0) }
+    var count by remember { mutableStateOf(0) }
 
     StatelessCounter(
         modifier = modifier,
-        count = waterCount,
-        onIncrement = { waterCount++ }
-    )
-    StatelessCounter(
-        modifier = modifier,
-        count = juiceCount,
-        onIncrement = { juiceCount++ }
+        count = count,
+        onIncrement = { count++ }
     )
 }
