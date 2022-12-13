bbcab966fb9d77deed4b547458fa014cc7a1210c
kangmingu93 <kangmingu93@gmail.com>
2022-12-13 21:50:34 +0900

    9. 상태 호이스팅

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -4,11 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -24,4 +21,41 @@ fun WaterCounter(modifier: Modifier = Modifier) {
             Text("Add one")
         }
     }
+}
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
+        Button(
+            modifier = Modifier.padding(top = 8.dp),
+            onClick = onIncrement,
+            enabled = count < 10
+        ) {
+            Text(text = "Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var waterCount by remember { mutableStateOf(0) }
+    var juiceCount by remember { mutableStateOf(0) }
+
+    StatelessCounter(
+        modifier = modifier,
+        count = waterCount,
+        onIncrement = { waterCount++ }
+    )
+    StatelessCounter(
+        modifier = modifier,
+        count = juiceCount,
+        onIncrement = { juiceCount++ }
+    )
 }
